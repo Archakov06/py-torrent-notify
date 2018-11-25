3f2349bdbe1bf8c41b173089660c37cbc009d4cd
Archakov Dennis <hello@archakov.im>
2018-11-25 03:35:17 +0300

    fix

--- index.py
+++ index.py
@@ -1,11 +1,16 @@
 import requests
 import re
+import sys
 from requests.auth import HTTPBasicAuth
+
+if len(sys.argv) == 1:
+    print('Please, set your bot token!')
+    exit()
 
 USER = '1'
 PASSWORD = '1'
 PORT = 9999
-
+TOKEN = sys.argv[1]
 URL = 'http://localhost:' + str(PORT)
 
 
@@ -15,13 +20,27 @@ def getTorrents():
     return data
 
 
-def sendMessage():
-    URL = 'https://api.telegram.org/botXXX:XXX/sendMessage'
+def serializeTorrentInfo(arr):
+    return {
+        "name": arr[2],
+        "size": arr[3],
+        "percent": float(arr[4]) / 10,
+        "downloaded": arr[5],
+        "peers": arr[13],
+        "seeds": arr[15],
+        "position": arr[17],
+    }
+
+
+def notifyMe(torrentName):
+    URL = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage'
     DATA = {
         "chat_id": 129070250,
-        "text": "Hello"
+        "text": "Загрузка торрента \"*{0}*\" завершена!".format(torrentName),
+        "parse_mode": "Markdown"
     }
     requests.post(url=URL, data=DATA)
 
 
-sendMessage()
+obj = serializeTorrentInfo(getTorrents()[2])
+notifyMe(obj['name'])
